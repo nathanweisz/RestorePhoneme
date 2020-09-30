8fd488b95e8cf2a81ed260b20738c815ccaba570
nathanweisz <nathanweisz@me.com>
2020-09-30 13:23:59 +0200

    first time load

---
+++ messaround/snipps_textGrid.m
@@ -0,0 +1,51 @@
+clear all
+restoredefaultpath
+
+addpath('~/Documents/GitHub/mPraat/')
+
+%%
+TextGridIn='02.TextGrid';
+WavFile='02.wav';
+tg = tgRead(TextGridIn);
+
+%%
+
+TarPhoneme = 'n';
+
+indTar=[];
+
+for ii=1:length(tg.tier{2}.Label)
+    if strcmpi(tg.tier{2}.Label(ii) ,TarPhoneme) && isempty(find(tg.tier{1}.T1 == tg.tier{2}.T1(ii))) %remove target phoneme at word onset
+        indTar = [indTar ii];
+    end
+end
+
+%%
+
+[wavMat, Fs] = audioread(WavFile);
+
+%%
+
+TarPoints = round([tg.tier{2}.T1(indTar); tg.tier{2}.T2(indTar)]' * Fs);
+
+%%
+
+wavMat_Trans = wavMat;
+
+
+for ii = 1:length(TarPoints)
+    nn=randn(size(wavMat_Trans(TarPoints(ii,1):TarPoints(ii,2),:)));
+    
+    %scale to be equal RMS to sound
+    nn_0 = nn ./ (rms(nn)/rms(wavMat));
+    
+    nn_3 = 10^(3/20) * nn_0;
+    nn_6 = 10^(6/20) * nn_0;
+    
+    wavMat_Trans(TarPoints(ii,1):TarPoints(ii,2),:)=nn_6;
+end
+
+%%
+
+audiowrite('test.wav', wavMat_Trans, Fs)
+
